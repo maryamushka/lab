e74afd074b75f26ffdd4b89b82827904a08458b8
maryamushka <mistermsks2004@gmail.com>
2025-02-15 21:33:07 +0300

    fix: Update main output message in main branch

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "utils.h"
 
 int main() {
-    std::cout << "сумма числа 2 и 3 равняется " << add(2, 3) << std::endl;
+    std::cout << "Сумма числа 2 и 3 равняется " << add(2, 3) << std::endl;
     std::cout << "5 - 2 = " << subtract(5, 2) << std::endl;
     std::cout << "4 * 3 = " << multiply(4, 3) << std::endl;
     std::cout << "8 / 2 = " << divide(8, 2) << std::endl;
